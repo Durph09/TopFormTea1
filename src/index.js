b51079a0dad440ed9030001781863b64425b180f
Durph09 <105114600+Durph09@users.noreply.github.com>
2023-03-15 21:00:24 -0400

    changed to hashbrowser

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
       
   <App />
-</BrowserRouter>
+</HashRouter>
   </React.StrictMode>
 );
 
